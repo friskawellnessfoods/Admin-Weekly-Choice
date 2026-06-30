85abde2fd8a250f9c968c83bac63fcd8d147e356
friskawellnessfoods <friskawellnessfoods@gmail.com>
2026-07-01 01:44:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbwk4JeWC4Pv7Veef6r80yTn60YcxiXQirRDMTe8cfiZtUXibD9J8Wa1KLg4eahEBeEW/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbxkiLhq0FSL0NInO1i_208jVNoqbHz1Aix_cySiCWauY-NvzUY8r_WyZ6X7zu2sn6KT/exec";
